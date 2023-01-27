6039d1c08c9c92a890236504d6147f83b88190cf
haideralwaqfi <haideralwaqfi@gmail.com>
2023-01-27 18:57:53 +0300

    final

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const setLoading = () => {
+    dispatch({ type: "SET_LOADING" });
+  };
+  const clearUsers = () => {
+    dispatch({ type: "CLEAR_USERS" });
+  };
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
